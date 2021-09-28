7033b91d0e5f058b8dcdef88999bbd3f35a2a5d3
jverswijver <jeroen.verswijver@gmail.com>
2021-09-28 08:57:33 -0500

    Boilerplate code generation from yaml file.

--- pharus/dynamic_api_gen.py
+++ pharus/dynamic_api_gen.py
@@ -3,14 +3,19 @@ from os import path
 
 # Exits the script without killing the python interpreter
 def populate_api():
+    
     f = open('pharus//dynamic_api.py', 'w')
+    y = open('pharus//dynamic_api_spec.yaml', 'r')
+    
+    valuesYaml = yaml.load(y, Loader=yaml.FullLoader)
     f.write(
 """
 from .server import app
 
 @app.route('/test', methods=['GET'])
 def test():
-    return 'it works!'
-"""
+
+    return '{field}'
+""".format(field=valuesYaml['Test'])
     )
     f.close()
