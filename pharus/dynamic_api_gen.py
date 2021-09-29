a4cc604d6d28d6d361dfc04dd25639eba7eac454
jverswijver <jeroen.verswijver@gmail.com>
2021-09-29 10:28:42 -0500

    Add framework for rest api generation from spec.

--- pharus/dynamic_api_gen.py
+++ pharus/dynamic_api_gen.py
@@ -1,21 +1,39 @@
 import yaml
-
+from textwrap import indent
 # Exits the script without killing the python interpreter
 def populate_api():
     
     f = open('pharus//dynamic_api.py', 'w')
     y = open('pharus//dynamic_api_spec.yaml', 'r')
     
-    valuesYaml = yaml.load(y, Loader=yaml.FullLoader)
-    f.write(
-"""
+    header_template = """
+# Auto-generated rest api
 from .server import app, protected_route
-from .interface import _DJConnector
+from .interface import _DJConnector, dj
 
-@app.route('/test', methods=['GET'])
+"""
+    f.write(header_template)
+
+    route_template = """
+
+@app.route('{route}', methods=['GET'])
 @protected_route
-def test(jwt_payload: dict) -> dict:
-    return str(_DJConnector._list_schemas(jwt_payload))
-""".format(field=valuesYaml['Test'])
+def test(jwt_payload: dict) -> list:
+    
+{query}
+
+    djconn = _DJConnector._set_datajoint_config(jwt_payload)
+    vm_dict = {{s: dj.VirtualModule(s, s, connection=djconn) for s in dj.list_schemas()}}
+
+    return dj_query(**vm_dict)
+"""
+
+    valuesYaml = yaml.load(y, Loader=yaml.FullLoader)
+    f.write(route_template.format(
+        route=valuesYaml['SciViz']['pages']
+        ['page2']['grids']['grid1']['components']['component1']['route'],
+        query=indent(valuesYaml['SciViz']['pages']
+        ['page2']['grids']['grid1']['components']['component1']['dj_query'], '    ')
+        )
     )
     f.close()
