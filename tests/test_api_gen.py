2aa55a5d8a71e7cb775e522a8ae5f6cb1095b6b0
jverswijver <jeroen.verswijver@gmail.com>
2021-09-30 14:50:33 -0500

    Fix test query.

--- tests/test_api_gen.py
+++ tests/test_api_gen.py
@@ -8,10 +8,12 @@ def test_auto_generated_route(token, client, schemas_simple):
     REST_response3 = client.get(f'/query3', headers=dict(Authorization=f'Bearer {token}'))
     REST_response4 = client.get(f'/query4', headers=dict(Authorization=f'Bearer {token}'))
 
-    expected_json = json.loads(
-        '[[1, 21, "Bernie", 7.77], [0, 10, "Raphael", 22.12], [0, 11, "Raphael", -1.21]]')
+    expected_json = (
+        '[[0, 11, "Raphael", -1.21], [1, 21, "Bernie", 7.77], [0, 10, "Raphael", 22.12]]')
 
-    assert expected_json == REST_response1.get_json(force=True)
-    assert expected_json == REST_response2.get_json(force=True)
-    assert expected_json == REST_response3.get_json(force=True)
-    assert expected_json == REST_response4.get_json(force=True)
+    print(json.dumps(REST_response1.get_json(force=True), sort_keys=True))
+
+    assert expected_json == json.dumps(REST_response1.get_json(force=True), sort_keys=True)
+    assert expected_json == json.dumps(REST_response2.get_json(force=True), sort_keys=True)
+    assert expected_json == json.dumps(REST_response3.get_json(force=True), sort_keys=True)
+    assert expected_json == json.dumps(REST_response4.get_json(force=True), sort_keys=True)
