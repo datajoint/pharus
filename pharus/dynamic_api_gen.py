d6ee8c3879e16850dc08baab940dff6e21438fbc
jverswijver <jeroen.verswijver@gmail.com>
2021-10-22 16:16:46 -0500

    Fix indentation.

--- pharus/dynamic_api_gen.py
+++ pharus/dynamic_api_gen.py
@@ -48,8 +48,8 @@ def {method_name}(jwt_payload: dict) -> dict:
     if request.method in {{'GET'}}:
         try:
             djconn = _DJConnector._set_datajoint_config(jwt_payload)
-            vm_dict = \\
-                {{s: dj.VirtualModule(s, s, connection=djconn) for s in dj.list_schemas()}}
+            vm_dict = {{s: dj.VirtualModule(s, s, connection=djconn) 
+                       for s in dj.list_schemas()}}
             query, fetch_args = dj_query(vm_dict)
             query = query & restriction()
             record_header, table_tuples, total_count = _DJConnector._fetch_records_by_query(
@@ -75,7 +75,7 @@ def {method_name}_attributes(jwt_payload: dict) -> dict:
         try:
             djconn = _DJConnector._set_datajoint_config(jwt_payload)
             vm_dict = {{s: dj.VirtualModule(s, s, connection=djconn)
-                        for s in dj.list_schemas()}}
+                       for s in dj.list_schemas()}}
             query, fetch_args = dj_query(vm_dict)
             attributes_meta = _get_attributes(query)
 
