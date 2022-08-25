84990bcb8830467fa11a491826138b87d5a7d402
A-Baji <bidabaji@gmail.com>
2022-08-25 14:20:04 -0500

    insert fail test

--- tests/test_form.py
+++ tests/test_form.py
@@ -5,6 +5,7 @@ from . import (
     connection,
     schemas_simple,
 )
+import datajoint as dj
 
 
 def test_insert(token, client, connection, schemas_simple):
@@ -15,6 +16,19 @@ def test_insert(token, client, connection, schemas_simple):
     )
     assert REST_response.status_code == 200
     assert REST_response.data == b"Insert successful"
+
+
+def test_insert_fail(token, client, connection, schemas_simple):
+    REST_response = client.post(
+        "/insert",
+        json=dict(a_id=1, b_id=32, b_number=1.23, c_id=400),
+        headers=dict(Authorization=f"Bearer {token}"),
+    )
+    assert REST_response.status_code == 500
+    assert (
+        dj.VirtualModule(schemas_simple[0].database, schemas_simple[0].database).TableB
+        & "b_id = 32"
+    ).fetch().size == 0
 
 
 def test_form_response(token, client, connection, schemas_simple):
