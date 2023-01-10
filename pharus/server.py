9a1551059a1854a315f5c768a87192459a5153b0
ana530 <chetanapitani@gmail.com>
2023-01-10 13:32:29 -0600

    version 0.7.1 bugfix

--- pharus/server.py
+++ pharus/server.py
@@ -126,7 +126,7 @@ def api_version() -> str:
             Content-Type: application/json
 
             {
-                "version": "0.7.0"
+                "version": "0.7.1"
             }
 
         :statuscode 200: No error.
