738a1f8b75684f08fd6f491738ad865fd99ae10c
Ethan Ho <53266718+ethho@users.noreply.github.com>
2024-04-19 17:18:19 -0500

    Revert "Test commit calling protected_route as decorator factory"
    This reverts commit be5b4dde3cbe2c6f22d8fc98a900e4097f2f1815.

--- pharus/dynamic_api_gen.py
+++ pharus/dynamic_api_gen.py
@@ -28,7 +28,7 @@ except (ModuleNotFoundError, ImportError):
     route_template = """
 
 @app.route('{route}', methods={rest_verb})
-@protected_route(include_user_obj=False)
+@protected_route
 def {method_name}(connection: dj.Connection) -> dict:
 
     if request.method in {rest_verb}:
