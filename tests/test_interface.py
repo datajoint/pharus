dc8ce34e15e4437ec824dae3bf564146b2bbde61
Ethan Ho <53266718+ethho@users.noreply.github.com>
2024-09-08 14:54:44 -0500

    Passing pytests for nei_nienborg_model.LabeledVideo.File

--- tests/test_interface.py
+++ tests/test_interface.py
@@ -1,5 +1,5 @@
 import os
-from typing import Optional
+from typing import Optional, Generator, Any
 import pytest
 import datajoint as dj
 from pharus.interface import _DJConnector as DJC
@@ -7,10 +7,10 @@ from . import get_schema_as_vm, get_db_creds
 
 
 @pytest.fixture
-def iub_lulab_devo_model_labeledvideo_file(connection: dj.Connection) -> Optional[dj.Table]:
+def nei_nienborg_model_labeledvideo_file(connection: dj.Connection) -> Generator[Optional[dj.Table], Any, Any]:
     with pytest.MonkeyPatch.context() as mp:
         mp.setenv('DJ_SUPPORT_FILEPATH_MANAGEMENT', 'TRUE')
-        vm = get_schema_as_vm('iub-lulab_devo_model', connection)
+        vm = get_schema_as_vm('nei_nienborg_model', connection)
         yield None if vm is None else vm.LabeledVideo.File
 
 
@@ -21,19 +21,16 @@ class TestDJConnector:
         assert djc is not None
 
     @pytest.mark.skipif(
-        (get_schema_as_vm('iub-lulab_devo_model', dj.conn(**get_db_creds())) is None),
+        (get_schema_as_vm('nei_nienborg_model', dj.conn(**get_db_creds())) is None),
         reason="Cannot access schema 'iub_lulab_devo_model' with these credentials"
     )
-    def test_can_fetch_filepath_attrs(self, iub_lulab_devo_model_labeledvideo_file: dj.Table, connection):
+    def test_can_fetch_filepath_attrs(self, nei_nienborg_model_labeledvideo_file: dj.Table, connection):
         """
         Tests _DJConnector._fetch_records for a table with a filepath attribute
 
         https://datajoint.atlassian.net/browse/PLAT-341
         """
-        table = iub_lulab_devo_model_labeledvideo_file
-        vm = get_schema_as_vm('iub-lulab_devo_model', connection)
-        breakpoint()
+        table = nei_nienborg_model_labeledvideo_file
         assert os.environ.get('DJ_SUPPORT_FILEPATH_MANAGEMENT').upper() == 'TRUE'
-        assert table.fetch(as_dict=True, limit=1)
-        # assert dj.list_schemas(connection=connection)
+        assert table.fetch('KEY', limit=1, download_path=None)
 
